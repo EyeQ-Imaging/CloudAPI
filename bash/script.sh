d7c525500e6034aa633abe4b0f3aa7f6180a9e45
ivan <ivan@eyeq.photos>
2024-03-01 23:16:36 +0300

    refactoring

--- bash/script.sh
+++ bash/script.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
+set -a # automatically export all variables
+source .env
+set +a # stop automatically exporting
+
+# Check if the APIKEY is empty
+if [ -z "$APIKEY" ]; then
+    echo "Error: The APIKEY is not set or it's empty in the .env file."
+    exit 1
+fi
+
+# Check if at least two arguments are provided
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 INPUT_FILE OUTPUT_FILE [CORRECTION_PARAMETERS]"
+    exit 1
+fi
+
+# Assign the first argument to INPUT_FILE and the second to OUTPUT_FILE
+INPUT_FILE=$1
+OUTPUT_FILE=$2
+
+# Check for the optional third argument
+if [ "$#" -ge 3 ]; then
+    CORRECTION_PARAMETERS=$3
+else
+    CORRECTION_PARAMETERS="default_value" # Replace this with any default value you want
+fi
+
+echo "Input file path: $INPUT_FILE"
+echo "Output file path: $OUTPUT_FILE"
+echo "Correction parameters: $CORRECTION_PARAMETERS"
+
 DOMAIN='https://api.perfectlyclear.io'
-APIKEY=''
-
-INPUT_URL='https://eyeq.photos/wp-content/uploads/2022/07/pfc-home-perfect-landscapes-before-min.jpg'
-INPUT_FILE='./input-image.jpg'
-OUTPUT_FILE='./output-image.jpg'
-
 FILE_TYPE='image'
-CORRECTION_PARAMETERS='&preset=Universal'
 
 function downloadFile {
     local url=$1
@@ -35,7 +59,7 @@ function uploadFile {
 
 function startCorrection {
     local fileKey=$1
-    local result=$(curl -s -H "X-API-KEY: $APIKEY" "$DOMAIN/v2/pfc?fileType=$FILE_TYPE&fileKey=$fileKey$CORRECTION_PARAMETERS")
+    local result=$(curl -s -H "X-API-KEY: $APIKEY" "$DOMAIN/v2/pfc?fileType=$FILE_TYPE&fileKey=$fileKey&$CORRECTION_PARAMETERS")
     local statusEndpoint=$(echo $result | jq -r '.statusEndpoint')
     echo $statusEndpoint
 }
@@ -56,7 +80,6 @@ IFS=$'\n' read -d '' -r -a pre_signed_info < <(getPreSignedURL && printf '\0')
 fileKey=${pre_signed_info[1]}
 uploadUrl=${pre_signed_info[2]}
 
-downloadFile "$INPUT_URL" "$INPUT_FILE"
 uploadFile "$INPUT_FILE" "$uploadUrl"
 
 status_endpoint=$(startCorrection "$fileKey")
